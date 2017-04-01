a281aa1b84cb47652c1b0107f9f1526c164be839
andela-aonifade <anuoluwapo.onifade@andela.com>
2017-04-01 19:53:06 +0100

    Modifies the index.html and karma.conf.js files

--- karma.conf.js
+++ karma.conf.js
@@ -69,7 +69,7 @@ module.exports = (config) => {
     // start these browsers
     // available browser launchers:
     // https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['Chrome'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
